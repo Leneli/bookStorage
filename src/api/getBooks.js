b8e6bf3b3b78d8abd963d784d0bc5f60b6f94f74
Elena Kuklina <liafell.lansera@gmail.com>
2023-05-23 23:38:10 +0300

    mongo connection

--- src/api/getBooks.js
+++ src/api/getBooks.js
@@ -1,15 +1,17 @@
-const {getStore} = require('../store');
+const BookModel = require('../models/bookModel');
 
-const getBooks = (id) => {
-  const store = getStore();
+const getBooks = async (id) => {
+  try {
+    if (id) {
+      const book = await BookModel.findById(id).select('-__v');
+      return book;
+    }
 
-  if (id) {
-    const book = store.books.find((item) => item.id === id);
-
-    return book;
+    const books = await BookModel.find().select('-__v');
+    return books;
+  } catch (error) {
+    console.log("🚀 ~ file: getBooks.js:13 ~ getBooks ~ error:", error);
   }
-
-  return store.books || [];
 };
 
 module.exports = {
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,5 @@
-const {createStore} = require('./createStore');
-const {getStore} = require('./getStore');
-const {updateStore} = require('./updateStore');
 const redisClient = require('./redis');
 
 module.exports = {
   redisClient,
-  createStore,
-  getStore,
-  updateStore,
 };
